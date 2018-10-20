873b8ccc81ae3eb0338c1f5d704763f65be15fc6
Nick Nothdurft <nothdurftnick@gmail.com>
2018-10-20 13:14:00 -0500

    Adding Data

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
 from flask_pymongo import PyMongo
 import pymongo
 import csv
 import json
 from bson import ObjectId
+import datetime as dt
 
-class JSONEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, ObjectId):
-            return str(o)
-        return json.JSONEncoder.default(self, o)
+# class JSONEncoder(json.JSONEncoder):
+#     def default(self, o):
+#         if isinstance(o, ObjectId):
+#             return str(o)
+#         return json.JSONEncoder.default(self, o)
 
 app = Flask(__name__)
 mongo = PyMongo(app, uri="mongodb://localhost:27017/test")
@@ -19,27 +20,32 @@ def index():
     data = {}
     headers = []
     count = 0
-    testdb = mongo.db.test
-    with open("companies.csv") as csvFile:
-        reader = csv.reader(csvFile)
-        for row in reader:
-            if count == 0:
-                headers = row
-            else:
-                data = {headers[0]:row[0],headers[1]:row[1],headers[2]:row[2], headers[3]:int(row[3])}
-                if testdb.find({headers[0]:row[0]}):
-                    testdb.update_one({headers[0]:row[0]}, {"$set":data}, upsert=True)
+    testdb = mongo.db.histData
+    files = ["2013.csv", "2014.csv", "2015.csv", "2016.csv", "2017.csv", "2018.csv"]
+    for f in files:
+        with open("S&P.csv") as csvFile:
+            reader = csv.reader(csvFile)
+            for row in reader:
+                if count == 0:
+                    headers = row
                 else:
-                    testdb.insert_one(data)
-            count += 1
+                    print(row[1])
+                    row[1] = row[1].replace(",", "")
+                    data = {headers[0]:row[0],headers[1]:float(row[1])}
+                    if testdb.find({headers[0]:row[0]}):
+                        testdb.update_one({headers[0]:row[0]}, {"$set":data}, upsert=True)
+                    else:
+                        testdb.insert_one(data)
+                count += 1
     newData = mongo.db.test.find()
     results = []
     for item in newData:
         print(item)
         results.append(item)
-    data = JSONEncoder().encode(results)
-    with open("data.json", "w") as outFile:
-        json.dump(data, outFile)
+    # data = JSONEncoder().encode(results)
+    # with open("data.json", "w") as outFile:
+    #     json.dump(data, outFile)
+    data = results
     print(data)
     return render_template("index.html", data = data)
 
