92d0227cf37399c3e7d0fa6035ad54689bea3b0d
Nick <nothdurftnick@gmail.com>
2018-10-22 11:17:20 -0500

    Database file

--- app.py
+++ app.py
@@ -1,53 +1,77 @@
 from flask import Flask, render_template, redirect, jsonify
 from flask_pymongo import PyMongo
-import pymongo
-import csv
-import json
-from bson import ObjectId
-import datetime as dt
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+import numpy
 
-# class JSONEncoder(json.JSONEncoder):
-#     def default(self, o):
-#         if isinstance(o, ObjectId):
-#             return str(o)
-#         return json.JSONEncoder.default(self, o)
+engine = create_engine("sqlite:///test.sqlite")
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+table13 = Base.classes.tests
+
+session = Session(engine)
 
 app = Flask(__name__)
-mongo = PyMongo(app, uri="mongodb://localhost:27017/test")
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/test")
 
-@app.route('/')
-def index():
-    data = {}
-    headers = []
-    count = 0
-    testdb = mongo.db.histData
-    files = ["2013.csv", "2014.csv", "2015.csv", "2016.csv", "2017.csv", "2018.csv"]
-    for f in files:
-        with open("S&P.csv") as csvFile:
-            reader = csv.reader(csvFile)
-            for row in reader:
-                if count == 0:
-                    headers = row
-                else:
-                    print(row[1])
-                    row[1] = row[1].replace(",", "")
-                    data = {headers[0]:row[0],headers[1]:float(row[1])}
-                    if testdb.find({headers[0]:row[0]}):
-                        testdb.update_one({headers[0]:row[0]}, {"$set":data}, upsert=True)
-                    else:
-                        testdb.insert_one(data)
-                count += 1
-    newData = mongo.db.test.find()
-    results = []
-    for item in newData:
-        print(item)
-        results.append(item)
-    # data = JSONEncoder().encode(results)
-    # with open("data.json", "w") as outFile:
-    #     json.dump(data, outFile)
-    data = results
-    print(data)
-    return render_template("index.html", data = data)
+results = session.query(table13.test1).all()
+all_names = list(np.ravel(results))
+print(all_names)
+# everything = []
+# print(results)
+# for thing in results:
+#     thisThing = {}
+#     thisThing["test1"] = results.test1
+#     thisThing["test2"] = results.test2
+#     everything.append(thisThing)
+
+# @app.route('/')
+# def test():
+#     results = session.query(table13).all()
+#     everything = []
+#     print(results)
+#     for thing in results:
+#         thisThing = {}
+#         thisThing["test1"] = results.test1
+#         thisThing["test2"] = results.test2
+#         everything.append(thisThing)
+#     # everything = list(np.ravel(results))
+#     return jsonify(everything)
+# def index():
+#     data = {}
+#     headers = []
+#     count = 0
+#     testdb = mongo.db.histData
+#     files = ["2013.csv", "2014.csv", "2015.csv", "2016.csv", "2017.csv", "2018.csv"]
+#     for f in files:
+#         with open("S&P.csv") as csvFile:
+#             reader = csv.reader(csvFile)
+#             for row in reader:
+#                 if count == 0:
+#                     headers = row
+#                 else:
+#                     print(row[1])
+#                     row[1] = row[1].replace(",", "")
+#                     data = {headers[0]:row[0],headers[1]:float(row[1])}
+#                     if testdb.find({headers[0]:row[0]}):
+#                         testdb.update_one({headers[0]:row[0]}, {"$set":data}, upsert=True)
+#                     else:
+#                         testdb.insert_one(data)
+#                 count += 1
+#     newData = mongo.db.test.find()
+#     results = []
+#     for item in newData:
+#         print(item)
+#         results.append(item)
+#     # data = JSONEncoder().encode(results)
+#     # with open("data.json", "w") as outFile:
+#     #     json.dump(data, outFile)
+#     data = results
+#     print(data)
+#     return render_template("index.html", data = data)
 
 @app.route('/loadCSV')
 def loadCSV():
