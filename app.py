90afeaadbe498bdf2dda86afcdcf4fdca98b3167
Nick <nothdurftnick@gmail.com>
2018-10-22 13:22:05 -0500

    Updating app.py

--- app.py
+++ app.py
@@ -1,86 +1,233 @@
 from flask import Flask, render_template, redirect, jsonify
-from flask_pymongo import PyMongo
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
-import numpy
+import numpy as np
 
-engine = create_engine("sqlite:///test.sqlite")
+engine = create_engine("sqlite:///stocks.sqlite")
 
 Base = automap_base()
 Base.prepare(engine, reflect=True)
-table13 = Base.classes.tests
-
-session = Session(engine)
 
 app = Flask(__name__)
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/test")
 
-results = session.query(table13.test1).all()
-all_names = list(np.ravel(results))
-print(all_names)
-# everything = []
-# print(results)
-# for thing in results:
-#     thisThing = {}
-#     thisThing["test1"] = results.test1
-#     thisThing["test2"] = results.test2
-#     everything.append(thisThing)
+@app.route('/')
+def index():
+    table = Base.classes.sp
+    session = Session(engine)
+    results = session.query(table).all()
+    tableList = []
+    for column in results:
+        tableData = {}
+        tableData["Date"] = column.Date
+        tableData["Price"] = column.Price
+        tableList.append(tableData)
+    return jsonify(tableList)
 
-# @app.route('/')
-# def test():
-#     results = session.query(table13).all()
-#     everything = []
-#     print(results)
-#     for thing in results:
-#         thisThing = {}
-#         thisThing["test1"] = results.test1
-#         thisThing["test2"] = results.test2
-#         everything.append(thisThing)
-#     # everything = list(np.ravel(results))
-#     return jsonify(everything)
-# def index():
-#     data = {}
-#     headers = []
-#     count = 0
-#     testdb = mongo.db.histData
-#     files = ["2013.csv", "2014.csv", "2015.csv", "2016.csv", "2017.csv", "2018.csv"]
-#     for f in files:
-#         with open("S&P.csv") as csvFile:
-#             reader = csv.reader(csvFile)
-#             for row in reader:
-#                 if count == 0:
-#                     headers = row
-#                 else:
-#                     print(row[1])
-#                     row[1] = row[1].replace(",", "")
-#                     data = {headers[0]:row[0],headers[1]:float(row[1])}
-#                     if testdb.find({headers[0]:row[0]}):
-#                         testdb.update_one({headers[0]:row[0]}, {"$set":data}, upsert=True)
-#                     else:
-#                         testdb.insert_one(data)
-#                 count += 1
-#     newData = mongo.db.test.find()
-#     results = []
-#     for item in newData:
-#         print(item)
-#         results.append(item)
-#     # data = JSONEncoder().encode(results)
-#     # with open("data.json", "w") as outFile:
-#     #     json.dump(data, outFile)
-#     data = results
-#     print(data)
-#     return render_template("index.html", data = data)
+@app.route('/year2013')
+def year2013():
+    table = Base.classes.year2013
+    session = Session(engine)
+    results = session.query(table).all()
+    tableList = []
+    for column in results:
+        tableData = {}
+        tableData["constituents"] = column.constituents
+        tableData["companyName"] = column.companyName
+        tableData["sector"] = column.sector
+        tableData["industryGroup"] = column.industryGroup
+        tableData["industry"] = column.industry
+        tableData["subIndustry"] = column.subIndustry
+        tableData["dividendAdjustedSplitAdjustedClosingStockPrice"] = column.dividendAdjustedSplitAdjustedClosingStockPrice
+        tableData["bevEbitdaMultiples"] = column.bevEbitdaMultiples
+        tableData["priceEarningsMultiples"] = column.priceEarningsMultiples
+        tableData["priceTangibleBook"] = column.priceTangibleBook
+        tableData["dividendYield"] = column.dividendYield
+        tableData["marketCapitalization"] = column.marketCapitalization
+        tableData["headquarterLocation"] = column.headquarterLocation
+        tableData["actual2018"] = column.actual2018
+        tableData["estimated2018"] = column.estimated2018
+        tableData["actual2017"] = column.actual2017
+        tableData["estimated2017"] = column.estimated2017
+        tableData["actual2016"] = column.actual2016
+        tableData["estimated2016"] = column.estimated2016
+        tableData["actual2015"] = column.actual2015
+        tableData["estimated2015"] = column.estimated2015
+        tableData["actual2014"] = column.actual2014
+        tableData["estimated2014"] = column.estimated2014
+        tableList.append(tableData)
+    return jsonify(tableList)
 
-@app.route('/loadCSV')
-def loadCSV():
-    data = {}
-    with open("Unit Rebuild.csv") as csvFile:
-        csvData = csv.reader(csvFile)
-        for item in csvData.iteritem:
-            print(item)
-    return data
+@app.route('/year2014')
+def year2014():
+    table = Base.classes.year2014
+    session = Session(engine)
+    results = session.query(table).all()
+    tableList = []
+    for column in results:
+        tableData = {}
+        tableData["constituents"] = column.constituents
+        tableData["companyName"] = column.companyName
+        tableData["sector"] = column.sector
+        tableData["industryGroup"] = column.industryGroup
+        tableData["industry"] = column.industry
+        tableData["subIndustry"] = column.subIndustry
+        tableData["dividendAdjustedSplitAdjustedClosingStockPrice"] = column.dividendAdjustedSplitAdjustedClosingStockPrice
+        tableData["bevEbitdaMultiples"] = column.bevEbitdaMultiples
+        tableData["priceEarningsMultiples"] = column.priceEarningsMultiples
+        tableData["priceTangibleBook"] = column.priceTangibleBook
+        tableData["dividendYield"] = column.dividendYield
+        tableData["marketCapitalization"] = column.marketCapitalization
+        tableData["headquarterLocation"] = column.headquarterLocation
+        tableData["actual2018"] = column.actual2018
+        tableData["estimated2018"] = column.estimated2018
+        tableData["actual2017"] = column.actual2017
+        tableData["estimated2017"] = column.estimated2017
+        tableData["actual2016"] = column.actual2016
+        tableData["estimated2016"] = column.estimated2016
+        tableData["actual2015"] = column.actual2015
+        tableData["estimated2015"] = column.estimated2015
+        tableData["actual2014"] = column.actual2014
+        tableData["estimated2014"] = column.estimated2014
+        tableList.append(tableData)
+    return jsonify(tableList)
+
+@app.route('/year2015')
+def year2015():
+    table = Base.classes.year2015
+    session = Session(engine)
+    results = session.query(table).all()
+    tableList = []
+    for column in results:
+        tableData = {}
+        tableData["constituents"] = column.constituents
+        tableData["companyName"] = column.companyName
+        tableData["sector"] = column.sector
+        tableData["industryGroup"] = column.industryGroup
+        tableData["industry"] = column.industry
+        tableData["subIndustry"] = column.subIndustry
+        tableData["dividendAdjustedSplitAdjustedClosingStockPrice"] = column.dividendAdjustedSplitAdjustedClosingStockPrice
+        tableData["bevEbitdaMultiples"] = column.bevEbitdaMultiples
+        tableData["priceEarningsMultiples"] = column.priceEarningsMultiples
+        tableData["priceTangibleBook"] = column.priceTangibleBook
+        tableData["dividendYield"] = column.dividendYield
+        tableData["marketCapitalization"] = column.marketCapitalization
+        tableData["headquarterLocation"] = column.headquarterLocation
+        tableData["actual2018"] = column.actual2018
+        tableData["estimated2018"] = column.estimated2018
+        tableData["actual2017"] = column.actual2017
+        tableData["estimated2017"] = column.estimated2017
+        tableData["actual2016"] = column.actual2016
+        tableData["estimated2016"] = column.estimated2016
+        tableData["actual2015"] = column.actual2015
+        tableData["estimated2015"] = column.estimated2015
+        tableData["actual2014"] = column.actual2014
+        tableData["estimated2014"] = column.estimated2014
+        tableList.append(tableData)
+    return jsonify(tableList)
+
+@app.route('/year2016')
+def year2016():
+    table = Base.classes.year2016
+    session = Session(engine)
+    results = session.query(table).all()
+    tableList = []
+    for column in results:
+        tableData = {}
+        tableData["constituents"] = column.constituents
+        tableData["companyName"] = column.companyName
+        tableData["sector"] = column.sector
+        tableData["industryGroup"] = column.industryGroup
+        tableData["industry"] = column.industry
+        tableData["subIndustry"] = column.subIndustry
+        tableData["dividendAdjustedSplitAdjustedClosingStockPrice"] = column.dividendAdjustedSplitAdjustedClosingStockPrice
+        tableData["bevEbitdaMultiples"] = column.bevEbitdaMultiples
+        tableData["priceEarningsMultiples"] = column.priceEarningsMultiples
+        tableData["priceTangibleBook"] = column.priceTangibleBook
+        tableData["dividendYield"] = column.dividendYield
+        tableData["marketCapitalization"] = column.marketCapitalization
+        tableData["headquarterLocation"] = column.headquarterLocation
+        tableData["actual2018"] = column.actual2018
+        tableData["estimated2018"] = column.estimated2018
+        tableData["actual2017"] = column.actual2017
+        tableData["estimated2017"] = column.estimated2017
+        tableData["actual2016"] = column.actual2016
+        tableData["estimated2016"] = column.estimated2016
+        tableData["actual2015"] = column.actual2015
+        tableData["estimated2015"] = column.estimated2015
+        tableData["actual2014"] = column.actual2014
+        tableData["estimated2014"] = column.estimated2014
+        tableList.append(tableData)
+    return jsonify(tableList)
+
+@app.route('/year2017')
+def year2017():
+    table = Base.classes.year2017
+    session = Session(engine)
+    results = session.query(table).all()
+    tableList = []
+    for column in results:
+        tableData = {}
+        tableData["constituents"] = column.constituents
+        tableData["companyName"] = column.companyName
+        tableData["sector"] = column.sector
+        tableData["industryGroup"] = column.industryGroup
+        tableData["industry"] = column.industry
+        tableData["subIndustry"] = column.subIndustry
+        tableData["dividendAdjustedSplitAdjustedClosingStockPrice"] = column.dividendAdjustedSplitAdjustedClosingStockPrice
+        tableData["bevEbitdaMultiples"] = column.bevEbitdaMultiples
+        tableData["priceEarningsMultiples"] = column.priceEarningsMultiples
+        tableData["priceTangibleBook"] = column.priceTangibleBook
+        tableData["dividendYield"] = column.dividendYield
+        tableData["marketCapitalization"] = column.marketCapitalization
+        tableData["headquarterLocation"] = column.headquarterLocation
+        tableData["actual2018"] = column.actual2018
+        tableData["estimated2018"] = column.estimated2018
+        tableData["actual2017"] = column.actual2017
+        tableData["estimated2017"] = column.estimated2017
+        tableData["actual2016"] = column.actual2016
+        tableData["estimated2016"] = column.estimated2016
+        tableData["actual2015"] = column.actual2015
+        tableData["estimated2015"] = column.estimated2015
+        tableData["actual2014"] = column.actual2014
+        tableData["estimated2014"] = column.estimated2014
+        tableList.append(tableData)
+    return jsonify(tableList)
+
+@app.route('/year2018')
+def year2018():
+    table = Base.classes.year2018
+    session = Session(engine)
+    results = session.query(table).all()
+    tableList = []
+    for column in results:
+        tableData = {}
+        tableData["constituents"] = column.constituents
+        tableData["companyName"] = column.companyName
+        tableData["sector"] = column.sector
+        tableData["industryGroup"] = column.industryGroup
+        tableData["industry"] = column.industry
+        tableData["subIndustry"] = column.subIndustry
+        tableData["dividendAdjustedSplitAdjustedClosingStockPrice"] = column.dividendAdjustedSplitAdjustedClosingStockPrice
+        tableData["bevEbitdaMultiples"] = column.bevEbitdaMultiples
+        tableData["priceEarningsMultiples"] = column.priceEarningsMultiples
+        tableData["priceTangibleBook"] = column.priceTangibleBook
+        tableData["dividendYield"] = column.dividendYield
+        tableData["marketCapitalization"] = column.marketCapitalization
+        tableData["headquarterLocation"] = column.headquarterLocation
+        tableData["actual2018"] = column.actual2018
+        tableData["estimated2018"] = column.estimated2018
+        tableData["actual2017"] = column.actual2017
+        tableData["estimated2017"] = column.estimated2017
+        tableData["actual2016"] = column.actual2016
+        tableData["estimated2016"] = column.estimated2016
+        tableData["actual2015"] = column.actual2015
+        tableData["estimated2015"] = column.estimated2015
+        tableData["actual2014"] = column.actual2014
+        tableData["estimated2014"] = column.estimated2014
+        tableList.append(tableData)
+    return jsonify(tableList)
 
 if __name__ == "__main__":
     app.run(debug=True)
